b6b09993ccfb1317f6e5a7f0181088fff2103ac6
DroidStudioAS <as.droiddevteam@gmail.com>
2024-03-25 07:39:06 +0100

    set up user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './user.schema';
+import { HttpModule } from '@nestjs/axios';
+import { RabbitService } from '../rabbit/rabbitmq.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, RabbitService],
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    HttpModule,
+  ],
 })
 export class UserModule {}
